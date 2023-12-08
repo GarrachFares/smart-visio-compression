a09b54d97ad3fe39b39a01b7f0a6778b8465d65c
ibrahimdevl <ibrahimhoussem2000@gmail.com>
2023-12-08 01:25:35 +0100

    image compression

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask
-from moviepy.editor import VideoFileClip
-#azure
+from flask import Flask, request, jsonify
 from azure.storage.blob import generate_blob_sas, BlobServiceClient, BlobSasPermissions
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import uuid
 import os
+from urllib.parse import urlparse
+from PIL import Image
+from moviepy.editor import VideoFileClip
+import requests
+from io import BytesIO
+
 
 def compress_video(input_path, output_path, bitrate='500k'):
     clip = VideoFileClip(input_path)
@@ -44,7 +48,7 @@ def store_file_to_bucket(file_path):
     permissions = BlobSasPermissions(read=True)  # Adjust permissions as needed
 
     # Generate SAS token
-    sas_token = generate_blob_sas(account_name=account_name, 
+    sas_token = generate_blob_sas(account_name=account_name,
                                 container_name=container_name, 
                                 blob_name=blob_name, 
                                 account_key=account_key,
@@ -70,9 +74,63 @@ def compress_video_router():
     file_path = store_file_to_bucket("output_compressed.mp4")
     return file_path    
 
-@app.route("/compress/image")
+@app.route("/compress/image", methods=["POST"])
 def compress_image_router():
-    # Example usage
-    compress_video("input.mp4", "output_compressed.mp4", bitrate='10k')
-    file_path = store_file_to_bucket("output_compressed.mp4")
-    return file_path        
+    url = request.json['url']
+    max_size_kb = request.json['max_size_kb']
+    parsed_url = urlparse(url)
+    image_name_with_extension = os.path.basename(parsed_url.path)
+    output_image_path, _ = os.path.splitext(image_name_with_extension)
+    output_image_path = output_image_path + ".webp"
+
+    # Call the imageCompress function
+    result = imageCompress(url, output_image_path, max_size_kb)
+    file_path = store_file_to_bucket(result[0])
+    os.remove(output_image_path)
+    # Return the results as JSON
+    return jsonify({
+        'compressed_url': file_path,
+        'input_weight': result[1],
+        'output_weight': result[2],
+        'input_dimensions': result[3],
+        'output_dimensions': result[4],
+        'input_format': result[5],
+        'output_format': result[6]
+    })
+
+
+def imageCompress(url, output_image_path, max_size_kb):
+    # Download the image file
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+
+    # Get input details
+    input_weight = len(response.content) // 1024  # Size in KB
+    input_dimensions = img.size
+    input_format = img.format
+
+    # Convert the image to RGB mode, as WebP format doesn't support RGBA
+    if img.mode == 'RGBA':
+        img = img.convert('RGB')
+
+    # Start with high quality
+    quality = 90
+
+    # Save the image again with reduced quality in WebP format
+    img.save(output_image_path, 'webp', quality=quality)
+
+    # While the image size is larger than the max size, reduce the quality
+    while os.path.getsize(output_image_path) > max_size_kb * 1024 and quality > 0:
+        quality -= 10  # Reduce quality by 10 each time
+        img.save(output_image_path, 'webp', quality=quality)
+
+    # Get output details
+    output_weight = os.path.getsize(output_image_path) // 1024  # Size in KB
+    output_dimensions = img.size
+    output_format = 'WEBP'
+
+    return output_image_path, input_weight, output_weight, input_dimensions, output_dimensions, input_format, output_format
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
